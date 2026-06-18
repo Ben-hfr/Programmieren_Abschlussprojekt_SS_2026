3209aa9558606cebab594b95635ced12f97cab7d
Fabian580 <kf5998@mci4me.at>
2026-06-18 15:18:15 +0200

    feat: implement get_total_distance method for calculating total traveled distance

--- src/gps_auswertung/calc_gps_data.py
+++ src/gps_auswertung/calc_gps_data.py
@@ -7,3 +7,41 @@ class Calc_GPS_Data():
     def __init__(self, gps_array: np.array):
         
         self.gps_array = gps_array
+
+    def get_total_distance(self) -> float:
+        """
+        takes: given Numpy array
+        does: this method calculates the traveled distance with the use of the Haversine formular.
+        It takes into account the given altitude.
+        gives: calculated total distance in meters. 
+        """
+
+        #Erd Raduis [M]
+        R = 63710000.0
+
+        #Lat/Long und höhe in einzelne Arrays speichern und Längen/Breiten -grad in Radiant umrechnen 
+        lat = np.radians(self.gps_array[:,0])
+        lon = np.radians(self.gps_array[:,1])
+        alt = self.gps_array[:,2]
+
+        #Calculates the delta i+1 and i with np.diff
+        dlat = np.diff(lat)
+        dlon = np.diff(lon)
+        dalt = np.diff(alt)
+
+        #define start and end 
+        lat_start = lat[:-1]
+        lat_end = lat[1:]
+
+        #Haversine formular
+        help_d = np.sqrt(np.sin(dlat/2)**2 + np.cos(lat_start) * np.cos(lat_end) * np.sin(dlon / 2)**2)
+        distance_2d = 2 * R * np.arcsin(help_d)
+
+        #3D Distance with altitude 
+        distance_3d = np.sqrt((distance_2d**2) + (dalt**2))
+
+        #calculte total distance by adding every part 
+        total_distance = np.sum(distance_3d)
+
+        return total_distance
+ 
